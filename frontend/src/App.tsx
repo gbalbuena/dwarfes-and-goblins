fd3c925bf2b0767646f0a4cf980d0598ab908125
Juan <juan@Juans-MacBook-Pro.local>
2022-05-05 21:41:49 +1000

    added react setup and moralis

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import { Button, ConnectButton } from 'web3uikit';
+
+export const App = () => (
+    <>
+        <Button theme="primary" type="button" text="Launch Dapp" />
+        <ConnectButton></ConnectButton>
+    </>
+);
+
