bd17d79be026dbaee46657f9c4f6fd3eea4612a8
0x1NotMe <nme15500@gmail.com>
2022-05-16 23:44:01 +1000

    feat(web3uikit): connect button

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,45 +1,13 @@
 import React, { useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { useMoralis } from "react-moralis";
+import { Button, ConnectButton } from 'web3uikit';
 
 function App() {
 
-    const { authenticate, isAuthenticated, isAuthenticating, user, account, logout } = useMoralis();
-
-    useEffect(() => {
-    if (isAuthenticated) {
-      // add your logic here
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isAuthenticated]);
-
-    const login = async () => {
-      if (!isAuthenticated) {
-
-        await authenticate({signingMessage: "Log in using Moralis" })
-          .then(function (user) {
-            console.log("logged in user:", user);
-            console.log(user!.get("ethAddress"));
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
-      }
-    }
-
-    const logOut = async () => {
-      await logout();
-      console.log("logged out");
-    }
-
-  return (
-    <div>
-      <h1>Moralis Hello World!</h1>
-      <button onClick={login}>Moralis Metamask Login</button>
-      <button onClick={logOut} disabled={isAuthenticating}>Logout</button>
-    </div>
-  );
+  return <>
+    <ConnectButton></ConnectButton>
+  </>
 }
 
 export default App;
