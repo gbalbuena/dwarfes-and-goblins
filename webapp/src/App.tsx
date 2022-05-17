196c21281b15edd021301726d024cf3bf6f76606
Juan Gabriel Balbuena Borba <gabrielbalbuena@gmail.com>
2022-05-17 20:49:40 +0800

    feature(ux): add bootstrap layout

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,13 +1,22 @@
 import React, { useEffect } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { useMoralis } from "react-moralis";
-import { Button, ConnectButton } from 'web3uikit';
+import { Avatar, ConnectButton } from 'web3uikit';
+import { Container, Row, Col } from 'react-bootstrap'
 
 function App() {
-
-  return <>
-    <ConnectButton></ConnectButton>
-  </>
+  return <Container className="AppWrapper">
+    <Row>
+      <Col><Avatar avatarBackground="#ff0000" fontSize={25} isRounded size={80} text="DG" theme="letters" />
+      </Col>
+      <Col xs={6}></Col>
+      <Col><ConnectButton></ConnectButton></Col>
+    </Row>
+    <Row>
+      <Col><h1>Hello world!</h1></Col>
+    </Row>
+  </Container>
 }
 
 export default App;
